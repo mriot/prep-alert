902b2c6b65fed88c5e12cd6b0e7b12975210f83d
mriot <kremer.markus1996@gmail.com>
2025-11-08 23:44:29 +0100

    add table to options pane that lists supported maps

--- src/UI/Settings/Settings.cpp
+++ src/UI/Settings/Settings.cpp
@@ -1,8 +1,12 @@
 #include "Settings.h"
 #include <Common/Globals.h>
 #include <Common/Utils.h>
+#include <Data/MapData.h>
 #include <Data/SettingsManager.h>
+#include <algorithm>
 #include <imgui/imgui.h>
+#include <string>
+#include <vector>
 
 void OnOptionsRender()
 {
@@ -67,4 +71,46 @@ void OnOptionsRender()
         ImGui::SetTooltip("Ctrl + Click to manually enter a value");
     }
     */
+
+    ImGui::Spacing();
+    ImGui::Separator();
+    ImGui::Spacing();
+
+    // supported maps table
+    std::string headerLabel = "Supported maps: " + std::to_string(G::SupportedMaps.size());
+
+    if (ImGui::CollapsingHeader(headerLabel.c_str()))
+    {
+        std::vector<std::pair<int, MapData>> entries;
+        entries.reserve(G::MapDataMap.size());
+
+        for (const auto &kv : G::MapDataMap)
+        {
+            entries.emplace_back(kv.first, kv.second);
+        }
+
+        std::sort(entries.begin(), entries.end(), [](const auto &a, const auto &b) {
+            return a.first < b.first;
+        });
+
+        ImGuiTableFlags tableFlags = ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg | ImGuiTableFlags_SizingFixedFit;
+        if (ImGui::BeginTable("SupportedMapsTable", 2, tableFlags))
+        {
+            ImGui::TableSetupColumn("ID", ImGuiTableColumnFlags_WidthFixed, 60.0f);
+            ImGui::TableSetupColumn("Map name", ImGuiTableColumnFlags_WidthStretch);
+            ImGui::TableHeadersRow();
+
+            for (const auto &entry : entries)
+            {
+                ImGui::TableNextRow();
+                ImGui::TableSetColumnIndex(0);
+                ImGui::Text("%d", entry.first);
+
+                ImGui::TableSetColumnIndex(1);
+                ImGui::TextUnformatted(entry.second.name.c_str());
+            }
+
+            ImGui::EndTable();
+        }
+    }
 }
