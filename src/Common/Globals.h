a3ca28f5c6a463b556245bc70d87e187a81be3d1
mriot <kremer.markus1996@gmail.com>
2025-12-12 14:03:26 +0100

    make Globals.h header-only by using inline variables

--- src/Common/Globals.h
+++ src/Common/Globals.h
@@ -10,28 +10,29 @@ namespace G
 {
     constexpr const char *ADDON_NAME = "PrepAlert";
 
-    extern HMODULE ModuleHandle;
+    inline HMODULE ModuleHandle = nullptr;
 
-    extern AddonAPI_t *APIDefs;
-    extern NexusLinkData_t *NexusLink;
-    extern Mumble::Data *MumbleLink;
-    extern Mumble::Identity *MumbleIdentity;
+    inline AddonAPI_t *APIDefs              = nullptr;
+    inline NexusLinkData_t *NexusLink       = nullptr;
+    inline Mumble::Data *MumbleLink         = nullptr;
+    inline Mumble::Identity *MumbleIdentity = nullptr;
 }
 
 namespace WorldState
 {
-    extern bool IsOnSupportedMap;
-    extern Continent CurrentContinent;
-    extern MapDefinition CurrentMapData;
-    extern uint32_t CurrentSectorID;
-    extern int CurrentMapFloor;
-    extern std::unordered_set<uint32_t> SupportedMaps;
-    extern std::unordered_map<int, MapDefinition> MapDataMap;
+    inline bool IsOnSupportedMap        = false;
+    inline Continent CurrentContinent   = Continent::Unknown;
+    inline MapDefinition CurrentMapData = {};
+    inline uint32_t CurrentSectorID     = 0;
+    inline int CurrentMapFloor          = 0;
+
+    inline std::unordered_set<uint32_t> SupportedMaps;
+    inline std::unordered_map<int, MapDefinition> MapDataMap;
 }
 
 namespace UIState
 {
     // set to true when options pane is open and reset to false each frame after overlay render
     // helps detect whether options pane is currently open
-    extern bool IsOptionsPaneOpen;
+    inline bool IsOptionsPaneOpen = false;
 }
